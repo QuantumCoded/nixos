0d131016b62b6ed8db7ee860333d30951b4184d4
QuantumCoded <bfields3@jscc.edu>
2023-05-21 00:13:31 -0500

    import home-manager module correctly

--- flake.nix
+++ flake.nix
@@ -32,7 +32,16 @@
           modules = [
             ./configuration.nix
             ./overlays.nix
-            ./home-manager.nix
+            
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.jeff = import ./home-manager.nix;
+
+              # Optionally, use home-manager.extraSpecialArgs to pass
+              # arguments to home-manager.nix
+            }
           ];
         };
       };
