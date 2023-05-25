b706dec773a8e8e0a60d63ec19ee30cbcd79bea7
QuantumCoded <bfields3@jscc.edu>
2023-05-25 03:24:17 -0500

    add 10 bspwm desktops

--- home/bspwm.nix
+++ home/bspwm.nix
@@ -2,5 +2,10 @@
 {
   xsession.windowManager.bspwm = {
     enable = true;
+
+    # TODO: This needs to be updated to handle multiple machines
+    monitors = {
+      eDP-1 = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" ];
+    };
   };
 }
