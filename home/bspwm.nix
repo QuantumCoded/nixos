880af46d7f6ee9f6a69c1b1428fb1c686180933f
QuantumCoded <bfields3@jscc.edu>
2023-05-26 16:57:28 -0500

    set the wallpaper for bspwm

--- home/bspwm.nix
+++ home/bspwm.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   xsession.windowManager.bspwm = {
     enable = true;
@@ -7,5 +7,11 @@
     monitors = {
       eDP-1 = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" ];
     };
+
+    # Stylix does this step in `xsession.initExtra`, but that will run any time X11 loads.
+    # Using xsession.windowManager.bspwm.extraConfig prevents the wallpaper from being set in plasma.
+    extraConfig = ''
+      ${pkgs.feh}/bin/feh  --no-fehbg --bg-scale ${../wallpaper.jpg}
+    '';
   };
 }
