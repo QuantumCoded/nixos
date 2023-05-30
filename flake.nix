8464fcb2980bc044c61f7d22976803c2255cb85c
QuantumCoded <bfields3@jscc.edu>
2023-05-30 17:03:15 -0500

    split common config out to common.nix

--- flake.nix
+++ flake.nix
@@ -21,76 +21,52 @@
     }:
     let
       system = "x86_64-linux";
+
+      commonModules = [
+        ./overlays.nix
+        ./common.nix
+        nur.hmModules.nur
+        base16.nixosModule
+        agenix.nixosModules.default
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.jeff = import ./home;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to ./home/default.nix
+        }
+      ];
+
+      specialArgs = {
+        inherit
+          self
+          system
+          nixpkgs
+          nixpkgs-unstable
+          nur
+          base16;
+        home = home-manager;
+      };
     in
     {
       nixosConfigurations = {
         quantum = nixpkgs.lib.nixosSystem {
-          inherit system;
-
-          specialArgs = {
-            inherit
-              self
-              system
-              nixpkgs
-              nixpkgs-unstable
-              nur
-              base16;
-            home = home-manager;
-          };
+          inherit system specialArgs;
 
           modules = [
             ./hosts/quantum/configuration.nix
-            ./overlays.nix
-
-            nur.hmModules.nur
-            base16.nixosModule
-            agenix.nixosModules.default
-
-            ./style
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.jeff = import ./home;
-
-              # Optionally, use home-manager.extraSpecialArgs to pass
-              # arguments to ./home/default.nix
-            }
-          ];
+          ] ++ commonModules;
         };
 
         odyssey = nixpkgs.lib.nixosSystem {
-          inherit system;
-
-          specialArgs = {
-            inherit
-              self
-              system
-              nixpkgs
-              nixpkgs-unstable
-              nur
-              base16;
-            home = home-manager;
-          };
+          inherit system specialArgs;
 
           modules = [
             ./hosts/odyssey/configuration.nix
-            ./overlays.nix
-
-            nur.hmModules.nur
-            base16.nixosModule
-
-            ./style
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.jeff = import ./home;
-
-              # Optionally, use home-manager.extraSpecialArgs to pass
-              # arguments to ./home/default.nix
-            }
-          ];
+          ] ++ commonModules;
         };
       };
     };
