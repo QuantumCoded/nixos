9c3eea47401ab2cbf1fa7abf159b99692b01a02e
QuantumCoded <bfields3@jscc.edu>
2023-05-31 00:52:50 -0500

    add missing fish functions

--- home/fish.nix
+++ home/fish.nix
@@ -1,6 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.fish = {
     enable = true;
+    plugins = [
+      { name = "tide"; src = pkgs.fishPlugins.tide.src; }
+    ];
+  };
+
+  # Load missing fish functions.
+  home.file = {
+    ".config/fish/functions/_tide_item_direnv.fish".source = ./fish-functions/_tide_item_direnv.fish;
+    ".config/fish/functions/_tide_item_distrobox.fish".source = ./fish-functions/_tide_item_distrobox.fish;
+    ".config/fish/functions/_tide_item_elixir.fish".source = ./fish-functions/_tide_item_elixir.fish;
+    ".config/fish/functions/_tide_item_gcloud.fish".source = ./fish-functions/_tide_item_gcloud.fish;
+    ".config/fish/functions/_tide_item_pulumi.fish".source = ./fish-functions/_tide_item_pulumi.fish;
   };
 }
