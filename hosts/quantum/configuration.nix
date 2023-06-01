fa1ccfc9981385970b8e4453fc6614fedde73a86
QuantumCoded <bfields3@jscc.edu>
2023-06-01 17:59:46 -0500

    enable nvida gpu drivers for quantum

--- hosts/quantum/configuration.nix
+++ hosts/quantum/configuration.nix
@@ -2,8 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
-
+{ ... }:
 {
   imports =
     [
@@ -12,4 +11,7 @@
     ];
 
   networking.hostName = "quantum"; # Define your hostname.
+
+  # Enable Nvidia drivers.
+  services.xserver.videoDrivers = [ "nvidia" ];
 }
