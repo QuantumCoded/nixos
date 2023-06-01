444668abf5a091cb066b5a6ba2a05cfa7b18c0ea
QuantumCoded <bfields3@jscc.edu>
2023-06-01 00:53:56 -0500

    use realpath in neofetch config for symlink issue

--- home/neofetch/default.nix
+++ home/neofetch/default.nix
@@ -4,7 +4,6 @@
   home.file.".config/neofetch/config.conf".source = ./config.conf;
   
   # Link the neofetch logo to the nix store.
-  # FIXME: neofetch seems to get really cranky when trying to load images from the nix
   home.file.".config/neofetch/logo.svg" = {
     source = ./logo.svg;
 
