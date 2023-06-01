cc1689c14ae7404aea1d037397b11fffa4491328
QuantumCoded <bfields3@jscc.edu>
2023-05-31 20:10:37 -0500

    remove unecessary fish functions

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -11,13 +11,4 @@
       ./uvars/tide.fish
     ];
   };
-
-  # Load missing fish functions.
-  home.file = {
-    ".config/fish/functions/_tide_item_direnv.fish".source = ./functions/_tide_item_direnv.fish;
-    ".config/fish/functions/_tide_item_distrobox.fish".source = ./functions/_tide_item_distrobox.fish;
-    ".config/fish/functions/_tide_item_elixir.fish".source = ./functions/_tide_item_elixir.fish;
-    ".config/fish/functions/_tide_item_gcloud.fish".source = ./functions/_tide_item_gcloud.fish;
-    ".config/fish/functions/_tide_item_pulumi.fish".source = ./functions/_tide_item_pulumi.fish;
-  };
 }
