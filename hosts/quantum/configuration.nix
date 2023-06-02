02fa87a30b724664a2a16fec1b14168bd585ede1
QuantumCoded <bfields3@jscc.edu>
2023-06-02 05:43:27 -0500

    lazy mount external hard drive

--- hosts/quantum/configuration.nix
+++ hosts/quantum/configuration.nix
@@ -14,4 +14,14 @@
 
   # Enable Nvidia drivers.
   services.xserver.videoDrivers = [ "nvidia" ];
+
+  # Lazy mount external hard drive.
+  fileSystems."/mnt" = {
+    device = "/dev/disk/by-uuid/a819667b-aff9-4946-971b-4b0df6d31880";
+    fsType = "ext4";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+    ];
+  };
 }
