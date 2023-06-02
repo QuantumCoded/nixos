d81717a76f924b23e5209d527638d74c3cfd7000
QuantumCoded <bfields3@jscc.edu>
2023-06-01 19:32:04 -0500

    downgrade kitty to 0.26.2 to fix neofetch icat

--- home/kitty.nix
+++ home/kitty.nix
@@ -1,7 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.kitty = {
     enable = true;
+    # FIXME: Downgrading kitty 0.28.1 -> 0.26.2 to fix icat rendering error
+    package = pkgs.racoon.kitty;
     settings = {
       scrollback_lines = 10000;
       enable_audio_bell = false;
