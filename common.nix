54934c937ff190c7057183568553cbb37a35abfc
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-06-04 17:49:01 -0500

    enable nixpkgs in nix repl in common.nix

--- common.nix
+++ common.nix
@@ -1,10 +1,13 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ pkgs, ... }:
-
+{ self, pkgs, ... }:
+with self.inputs;
 {
-  # Enable secret service
+  # Enable nixpkgs in nix repl.
+  nix.registry.nixpkgs.flake = nixpkgs;
+
+  # Enable secret service.
   services.passSecretService.enable = true;
 
   # Enable OpenGL for kitty terminal.
