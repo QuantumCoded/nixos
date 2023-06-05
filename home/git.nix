2b4ac954e11915fad94e2b5429d23c581606051c
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-06-04 20:30:02 -0500

    add oops and trash aliases to git

--- home/git.nix
+++ home/git.nix
@@ -4,11 +4,19 @@ let
 in
 {
   programs.git = {
-    inherit userName;
     enable = true;
+
+    inherit userName;
     userEmail = "${userName}@users.noreply.github.com";
+
     signing.key = config.home.homeDirectory + "/.ssh/id_ed25519";
     signing.signByDefault = true;
+
+    aliases = {
+      oops = "reset --soft HEAD~1";
+      trash = "reset --hard HEAD";
+    };
+
     extraConfig = {
       gpg.format = "ssh";
       pull.rebase = false;
