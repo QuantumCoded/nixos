e6e31be947dbecfff967df2873f7c384730e7230
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-06-06 03:39:32 -0500

    configure displays for quantum

--- hosts/quantum/configuration.nix
+++ hosts/quantum/configuration.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -24,4 +24,10 @@
       "noauto"
     ];
   };
+
+  # Configure the displays.
+  # TODO: refactor to look better, splitting this with either \ or \\ DOES NOT WORK!
+  services.xserver.displayManager.setupCommands = ''
+    ${pkgs.xorg.xrandr}/bin/xrandr --output DP-0 --mode 1920x1080 --rate 144 --pos 0x0 --output DP-2 --primary --mode 1920x1080 --rate 144 --right-of DP-0 --output DP-4 --mode 1920x1080 --rate 144 --right-of DP-2 --output HDMI-0 --mode 1920x1080 --rate 60 --right-of DP-4
+  '';
 }
