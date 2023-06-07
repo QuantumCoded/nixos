96761eabecf7b805803c1ee5c857b8184c5f8fd7
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-06-07 00:57:00 -0500

    add raccoon and unstable to inputs

--- common.nix
+++ common.nix
@@ -1,11 +1,13 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ self, pkgs, ... }:
-with self.inputs;
+{ inputs, pkgs, ... }:
+
 {
   # Enable nixpkgs in nix repl.
-  nix.registry.nixpkgs.flake = nixpkgs;
+  nix.registry.nixpkgs.flake = inputs.nixpkgs;
+  nix.registry.racoon.flake = inputs.nixpkgs-racoon;
+  nix.registry.unstable.flake = inputs.nixpkgs-unstable;
 
   # Enable secret service.
   services.passSecretService.enable = true;
