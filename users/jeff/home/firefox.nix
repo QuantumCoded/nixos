516664325acb64620f0eeee41936a4a50223cc7e
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-06-10 00:57:16 -0500

    add some firefox settings and install darkreader

--- users/jeff/home/firefox.nix
+++ users/jeff/home/firefox.nix
@@ -33,9 +33,10 @@ in
 {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox;
+
     profiles.default = {
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        darkreader
         ublock-origin
         return-youtube-dislikes
         web-scrobbler
@@ -54,6 +55,42 @@ in
           };
         })
       ];
+
+      settings = {
+        # Don't save passwords.
+        "signon.rememberSignons" = false;
+
+        # Don't show a warning when opening about:config. I know what I'm doing!
+        "browser.aboutConfig.showWarning" = false;
+
+        # Disable telemetry.
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+        "browser.ping-centre.telemetry" = false;
+        "browser.tabs.crashReporting.sendReport" = false;
+        "devtools.onboarding.telemetry.logged" = false;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.server" = "";
+        "toolkit.telemetry.unified" = false;
+
+        # Disable Firefox Pocket.
+        "extensions.pocket.enabled" = false;
+        "browser.newtabpage.activity-stream.feeds.discoverystreamfeed" = false;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+        "browser.newtabpage.activity-stream.feeds.topsites" = false;
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+
+        # Disable Firefox View.
+        "browser.tabs.firefox-view" = false;
+
+        # Disable Firefox account features
+	      "identity.fxaccounts.enabled" = false;
+
+        # FIXME: Enable dark theme.
+        "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+        "browser.theme.content-theme" = 2;
+        "browser.theme.toolbar-theme" = 2;
+      };
     };
   };
 }
