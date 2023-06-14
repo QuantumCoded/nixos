61ad9b901ab2546dc73bce067d0cb58df5fb7634
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-06-14 07:08:35 -0500

    restore packages

---
+++ packages/default.nix
@@ -0,0 +1,10 @@
+{ inputs, ... }:
+
+inputs.utils.lib.eachDefaultSystem (system:
+let
+  pkgs = import inputs.nixpkgs { inherit system; };
+in
+{
+  packages.gdlauncher = pkgs.callPackage ./gdlauncher.nix { };
+  packages.xwinwrap = pkgs.callPackage ./xwinwrap.nix { };
+})
