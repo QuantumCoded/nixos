573b7345588a795ae88838adcbf431c535f99954
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-06-16 04:56:36 -0500

    add autoNumlock to sddm for plasma desktop

--- base/host/services/xserver/plasma.nix
+++ base/host/services/xserver/plasma.nix
@@ -1,8 +1,13 @@
 { withSddm ? true }:
-{ ... }:
+{ lib, ... }:
 
 {
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = withSddm;
   services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager = lib.mkIf withSddm {
+    sddm = {
+      enable = true;
+      autoNumlock = true;
+    };
+  };
 }
