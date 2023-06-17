45c6ffd2e480f922c67f07eb304e7ab31d9c8747
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-06-17 04:45:08 -0500

    enable unfree nixpkgs in flake packages

--- packages/default.nix
+++ packages/default.nix
@@ -2,7 +2,10 @@
 
 inputs.utils.lib.eachDefaultSystem (system:
 let
-  pkgs = import inputs.nixpkgs { inherit system; };
+  pkgs = import inputs.nixpkgs {
+    inherit system;
+    config.allowUnfree = true;
+  };
 in
 {
   packages.gdlauncher = pkgs.callPackage ./gdlauncher.nix { };
