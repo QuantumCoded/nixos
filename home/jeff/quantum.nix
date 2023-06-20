b4bd3996b73ed43a6c8baaebf362292ea348c8ac
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-06-20 01:52:34 -0500

    change home config for quantum back

--- home/jeff/quantum.nix
+++ home/jeff/quantum.nix
@@ -1,40 +1,46 @@
-{ pkgs, ... }: {
+{ ... }:
+
+{
   imports = [
-    # Home
-    (import ../../base/user/home/fish { })
-    (import ../../base/user/home/neofetch { })
-    (import ../../base/user/home/bspwm.nix {
-      monitors = {
-        DP-0 = [ "L1" "L2" "L3" ];
-        DP-2 = [ "M1" "M2" "M3" ];
-        DP-4 = [ "R1" "R2" "R3" ];
-        HDMI-0 = [ "P" ];
-      };
+    ({ pkgs, ... }: {
+      imports = [
+        # Home
+        (import ../../base/user/home/fish { })
+        (import ../../base/user/home/neofetch { })
+        (import ../../base/user/home/bspwm.nix {
+          monitors = {
+            DP-0 = [ "L1" "L2" "L3" ];
+            DP-2 = [ "M1" "M2" "M3" ];
+            DP-4 = [ "R1" "R2" "R3" ];
+            HDMI-0 = [ "P" ];
+          };
 
-      extraConfig = ''
-        ${pkgs.dunst}/bin/dunst &
-        ${pkgs.procps}/bin/pidof xwinwrap || ${pkgs.flake.xwinwrap}/bin/xwinwrap -fs -fdt -ni -b -nf -un -o 1.0 -- ${pkgs.mpv}/bin/mpv -wid WID --loop --no-audio ${../../wallpapers/animated.mkv}
-      '';
+          extraConfig = ''
+            ${pkgs.dunst}/bin/dunst &
+            ${pkgs.procps}/bin/pidof xwinwrap || ${pkgs.flake.xwinwrap}/bin/xwinwrap -fs -fdt -ni -b -nf -un -o 1.0 -- ${pkgs.mpv}/bin/mpv -wid WID --loop --no-audio ${../../wallpapers/animated.mkv}
+          '';
+        })
+        (import ../../base/user/home/dunst.nix {
+          monitor = 1;
+          notificationLimit = 3;
+          origin = "bottom-left";
+        })
+        (import ../../base/user/home/firefox.nix { })
+        (import ../../base/user/home/git.nix { userName = "QuantumCoded"; })
+        (import ../../base/user/home/home-manager.nix { userName = "jeff"; })
+        (import ../../base/user/home/kitty.nix { package = pkgs.raccoon.kitty; })
+        (import ../../base/user/home/rofi.nix { })
+        (import ../../base/user/home/sxhkd.nix { desktopOrder = "1,4,7,2,5,8,3,6,9,10"; })
+        (import ../../base/user/home/vscode.nix { fontSize = 15; })
+
+        # Style
+        (import ../../base/user/style/kitty.nix { })
+        (import ../../base/user/style/scheme.nix { })
+
+        # Overlays
+        (import ../../base/overlays.nix { })
+      ];
     })
-    (import ../../base/user/home/dunst.nix {
-      monitor = 1;
-      notificationLimit = 3;
-      origin = "bottom-left";
-    })
-    (import ../../base/user/home/firefox.nix { })
-    (import ../../base/user/home/git.nix { userName = "QuantumCoded"; })
-    (import ../../base/user/home/home-manager.nix { userName = "jeff"; })
-    (import ../../base/user/home/kitty.nix { package = pkgs.raccoon.kitty; })
-    (import ../../base/user/home/rofi.nix { })
-    (import ../../base/user/home/sxhkd.nix { desktopOrder = "1,4,7,2,5,8,3,6,9,10"; })
-    (import ../../base/user/home/vscode.nix { fontSize = 15; })
-
-    # Style
-    (import ../../base/user/style/kitty.nix { })
-    (import ../../base/user/style/scheme.nix { })
-
-    # Overlays
-    (import ../../base/overlays.nix { })
   ];
 
   home.stateVersion = "23.05";
