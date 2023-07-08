7949ee9cb46a275cfcbb952bc3cd3ec13c9a8a00
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-07-08 09:02:36 -0500

    redo hardware configurations to include storage

---
+++ hardware/quantum/storage.nix
@@ -0,0 +1,51 @@
+{ ... }:
+
+{
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/root";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot/efi" = {
+    device = "/dev/disk/by-label/EFI";
+    fsType = "vfat";
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/disk/by-label/home";
+    fsType = "ext4";
+  };
+
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-label/nix";
+    fsType = "ext4";
+  };
+
+  fileSystems."/var" = {
+    device = "/dev/disk/by-label/var";
+    fsType = "ext4";
+  };
+
+  fileSystems."/mnt" = {
+    device = "/dev/disk/by-label/external";
+    fsType = "ext4";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+    ];
+  };
+
+  fileSystems."/nfs/hydrogen" = {
+    device = "docker.vmlan:/data";
+    fsType = "nfs";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.idle-timeout=600"
+    ];
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-label/swap"; }
+  ];
+}
