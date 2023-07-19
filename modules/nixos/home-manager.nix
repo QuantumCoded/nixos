6b5470146a35774044c72865434ceffb24a5619d
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-07-18 20:19:52 -0500

    add top level ways to pass in home-manager attrs

--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -3,41 +3,62 @@ let
   inherit (lib)
     mkEnableOption
     mkIf
+    mkMerge
     mkOption
     recursiveUpdate
     types
     ;
 
-  cfg = config.base.user.jeff;
+  cfg = config.base;
 in
 {
   imports = [ inputs.home-manager.nixosModules.home-manager ];
 
-  options.base.user.jeff = {
-    enable = mkEnableOption "Jeff User";
+  options.base = {
     homeConfig = mkOption {
       type = types.attrs;
       default = { };
     };
-    baseConfig = mkOption {
+
+    homeBaseConfig = mkOption {
       type = types.attrs;
       default = { };
     };
+
+    user.jeff = {
+      enable = mkEnableOption "Jeff User";
+
+      homeConfig = mkOption {
+        type = types.attrs;
+        default = { };
+      };
+
+      baseConfig = mkOption {
+        type = types.attrs;
+        default = { };
+      };
+    };
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf cfg.user.jeff.enable {
     home-manager.extraSpecialArgs = { inherit inputs self; };
     home-manager.useGlobalPkgs = true;
     home-manager.useUserPackages = true;
-    home-manager.users.jeff = recursiveUpdate cfg.homeConfig {
-      imports = [
-        ../home
-        (../../home/jeff + "/${config.networking.hostName}.nix")
-      ];
+    home-manager.users.jeff = mkMerge [
+      cfg.homeConfig
+      cfg.user.jeff.homeConfig
+      {
+        imports = [
+          ../home
+          (../../home/jeff + "/${config.networking.hostName}.nix")
+        ];
 
-      programs.home-manager.enable = true;
+        programs.home-manager.enable = true;
 
-      base = cfg.baseConfig;
-    };
+        base = recursiveUpdate
+          cfg.homeBaseConfig
+          cfg.user.jeff.baseConfig;
+      }
+    ];
   };
 }
