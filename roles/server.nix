ca2128756924422150ab0b674186f1f0bce08c06
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-07-19 18:56:36 -0500

    add server role

---
+++ roles/server.nix
@@ -0,0 +1,59 @@
+{ pkgs, ... }:
+
+{
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = false;
+  };
+
+  services.openssh.enable = true;
+
+  users.users.jeff = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.fish;
+
+    packages = [ ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    btop
+    git
+    micro
+    tmux
+    tree
+    vim
+    wget
+    xclip
+  ];
+
+  i18n = rec {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = defaultLocale;
+      LC_IDENTIFICATION = defaultLocale;
+      LC_MEASUREMENT = defaultLocale;
+      LC_MONETARY = defaultLocale;
+      LC_NAME = defaultLocale;
+      LC_NUMERIC = defaultLocale;
+      LC_PAPER = defaultLocale;
+      LC_TELEPHONE = defaultLocale;
+      LC_TIME = defaultLocale;
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  base.boot.enable = true;
+  base.flakes.enable = true;
+
+  base.user.jeff.enable = true;
+
+  system.stateVersion = "23.05";
+}
