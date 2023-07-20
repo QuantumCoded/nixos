9e18d0a72147f20fdbcc61e6f0fa3b51db597e57
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-07-19 21:16:40 -0500

    enable minecraft servers

--- roles/server.nix
+++ roles/server.nix
@@ -1,5 +1,7 @@
-{ pkgs, ... }:
-
+{ lib, pkgs, ... }:
+let
+  inherit (lib) concatStringsSep;
+in
 {
   sound.enable = true;
   hardware.pulseaudio.enable = false;
@@ -64,6 +66,58 @@
   };
 
   base.user.jeff.enable = true;
+
+  base.minecraft.enable = true;
+  base.minecraft.servers =
+    let
+      jvmOpts = concatStringsSep " " [
+        "-server"
+        "-Xms4G"
+        "-Xmx8G"
+        "-XX:+UseG1GC"
+        "-XX:+ParallelRefProcEnabled"
+        "-XX:MaxGCPauseMillis=200"
+        "-XX:+UnlockExperimentalVMOptions"
+        "-XX:+DisableExplicitGC"
+        "-XX:+AlwaysPreTouch"
+        "-XX:G1NewSizePercent=30"
+        "-XX:G1MaxNewSizePercent=40"
+        "-XX:G1HeapRegionSize=8M"
+        "-XX:G1ReservePercent=20"
+        "-XX:G1HeapWastePercent=5"
+        "-XX:G1MixedGCCountTarget=4"
+        "-XX:InitiatingHeapOccupancyPercent=15"
+        "-XX:G1MixedGCLiveThresholdPercent=90"
+        "-XX:G1RSetUpdatingPauseTimePercent=5"
+        "-XX:SurvivorRatio=32"
+        "-XX:+PerfDisableSharedMem"
+        "-XX:MaxTenuringThreshold=1"
+      ];
+    in
+    {
+      nomifactory = {
+        inherit jvmOpts;
+        port = 25566;
+        preStart = '''';
+        restart = false;
+        jar = "forge-1.12.2-14.23.5.2860.jar";
+        jre = pkgs.openjdk8;
+      };
+
+      vanilla = {
+        inherit jvmOpts;
+        port = 25565;
+        restart = false;
+      };
+    };
+
+  # ankisyncd
+  # ftp
+  # gitea
+  # invidious
+  # jellyfin
+  # searx
+  # syncthing
 
   system.stateVersion = "23.05";
 }
