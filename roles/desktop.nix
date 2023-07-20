648a65c6fb6bcf4c3741495548cd87ed26d52b8d
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-07-20 11:29:37 -0500

    enable minecraft server on desktop for testing

--- roles/desktop.nix
+++ roles/desktop.nix
@@ -1,5 +1,7 @@
-{ pkgs, ... }:
-
+{ lib, pkgs, ... }:
+let
+  inherit (lib) concatStringsSep;
+in
 {
   hardware.opengl = {
     # TODO: might need to downgrade for kitty if it's not fixed yet
@@ -130,6 +132,38 @@
   base.user.jeff.enable = true;
   base.user.jeff.baseConfig = {
     fish.tide.enable = true;
+  };
+
+  base.minecraft.enable = true;
+  base.minecraft.servers.nomifactory = {
+    port = 25565;
+    preStart = '''';
+    restart = false;
+    jar = "forge-1.12.2-14.23.5.2860.jar";
+    jre = pkgs.openjdk8;
+    jvmOpts = concatStringsSep " " [
+      "-server"
+      "-Xms4G"
+      "-Xmx8G"
+      "-XX:+UseG1GC"
+      "-XX:+ParallelRefProcEnabled"
+      "-XX:MaxGCPauseMillis=200"
+      "-XX:+UnlockExperimentalVMOptions"
+      "-XX:+DisableExplicitGC"
+      "-XX:+AlwaysPreTouch"
+      "-XX:G1NewSizePercent=30"
+      "-XX:G1MaxNewSizePercent=40"
+      "-XX:G1HeapRegionSize=8M"
+      "-XX:G1ReservePercent=20"
+      "-XX:G1HeapWastePercent=5"
+      "-XX:G1MixedGCCountTarget=4"
+      "-XX:InitiatingHeapOccupancyPercent=15"
+      "-XX:G1MixedGCLiveThresholdPercent=90"
+      "-XX:G1RSetUpdatingPauseTimePercent=5"
+      "-XX:SurvivorRatio=32"
+      "-XX:+PerfDisableSharedMem"
+      "-XX:MaxTenuringThreshold=1"
+    ];
   };
 
   system.stateVersion = "23.05";
