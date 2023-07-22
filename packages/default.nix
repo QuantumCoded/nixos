e6659b92b898c187378cb21f27e499080bce16a6
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-07-21 21:09:38 -0500

    add nondescript music server to package index

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,7 @@
 { nixpkgs, inputs, ... }:
 
 inputs.flake-utils.lib.eachDefaultSystem (system: {
+  packages.dmx-server = nixpkgs.callPackage ./dmx-server.nix { };
   packages.gdlauncher = nixpkgs.callPackage ./gdlauncher.nix { };
   packages.xwinwrap = nixpkgs.callPackage ./xwinwrap.nix { };
 })
