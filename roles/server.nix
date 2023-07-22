28297295df5bb1d206bd9e8f588897552f52536b
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-07-22 02:58:29 -0500

    change acme ca

--- roles/server.nix
+++ roles/server.nix
@@ -168,7 +168,7 @@ in
 
   services.caddy = {
     enable = true;
-    acmeCA = "https://acme-staging-v02.api.letsencrypt.org/directory";
+    acmeCA = "https://acme-v02.api.letsencrypt.org/directory";
     virtualHosts = {
       "gitea.hydrogen.lan".extraConfig = "reverse_proxy http://127.0.0.1:3000";
     };
