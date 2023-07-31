77ea1605102606bd0ff47174998489a842fd205b
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-07-30 23:29:36 -0500

    add nomos-rebuild to packages index

--- packages/default.nix
+++ packages/default.nix
@@ -4,5 +4,6 @@ inputs.flake-utils.lib.eachDefaultSystem (system: {
   packages.airsonic-advanced = nixpkgs.callPackage ./airsonic-advanced.nix { };
   packages.dmx-server = nixpkgs.callPackage ./dmx-server { };
   packages.gdlauncher = nixpkgs.callPackage ./gdlauncher.nix { };
+  packages.nomos-rebuild = nixpkgs.callPackage ./nomos-rebuild { };
   packages.xwinwrap = nixpkgs.callPackage ./xwinwrap.nix { };
 })
