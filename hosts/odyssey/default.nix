dbf332960a94111fe4bd8b093baed4c161d61da2
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-07-31 23:00:56 -0500

    add display configuration for odyssey

--- hosts/odyssey/default.nix
+++ hosts/odyssey/default.nix
@@ -7,4 +7,10 @@
   ];
 
   networking.hostName = "odyssey";
+
+  base.homeConfig = {
+    xsession.windowManager.bspwm.monitors = {
+      eDP-1 = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" ];
+    };
+  };
 }
