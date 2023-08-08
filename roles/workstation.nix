f60eaf3650324104d7f91f25129a2d4b3f44ff32
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-08-08 06:36:02 -0500

    enable flatpaks until i package discord-screenaudio

--- roles/workstation.nix
+++ roles/workstation.nix
@@ -88,6 +88,15 @@
     ];
   };
 
+  services.flatpak.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
+
   environment.systemPackages = with pkgs; [
     btop
     git
