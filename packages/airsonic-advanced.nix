ea1e1c1f3c53a7c7824a97726dfc17de2efa0291
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-08-25 20:34:09 -0500

    bump airsonic-advanced

--- packages/airsonic-advanced.nix
+++ packages/airsonic-advanced.nix
@@ -1,12 +1,12 @@
 { airsonic, fetchurl }:
 
-airsonic.overrideAttrs (final: prev: rec {
+airsonic.overrideAttrs (final: prev: {
   pname = "airsonic-advanced";
-  version = "10.6.0";
+  version = "11.0.0-snapshot-20230217142243";
 
   src = fetchurl {
-    url = "https://github.com/airsonic-advanced/airsonic-advanced/releases/download/v${version}/airsonic.war";
-    hash = "sha256-Uo9BfllJkWGtf9i4bTkePH7/cOCCVGXjFfhtNBdmTGk=";
+    url = "https://github.com/airsonic-advanced/airsonic-advanced/releases/download/11.0.0-SNAPSHOT.20230217142243/airsonic.war";
+    hash = "sha256-hNN9NFhqoqYzWgIU10OgUY6C19Zo6MwUKrsS4gIRIg8=";
   };
 
   meta.homepage = "https://github.com/airsonic-advanced";
