09e6446906d29ac2a502f2514ce3f55f2f1cc8e4
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-08-29 14:45:59 -0500

    add minecraft server jvm options

--- roles/server.nix
+++ roles/server.nix
@@ -79,6 +79,28 @@ in
     airsonic = {
       enable = true;
       jre = pkgs.openjdk11;
+      jvmOptions = [
+        "-server"
+        "-Xms4G"
+        "-XX:+UseG1GC"
+        "-XX:+ParallelRefProcEnabled"
+        "-XX:MaxGCPauseMillis=200"
+        "-XX:+UnlockExperimentalVMOptions"
+        "-XX:+DisableExplicitGC"
+        "-XX:+AlwaysPreTouch"
+        "-XX:G1NewSizePercent=30"
+        "-XX:G1MaxNewSizePercent=40"
+        "-XX:G1HeapRegionSize=16M"
+        "-XX:G1ReservePercent=20"
+        "-XX:G1HeapWastePercent=5"
+        "-XX:G1MixedGCCountTarget=4"
+        "-XX:InitiatingHeapOccupancyPercent=25"
+        "-XX:G1MixedGCLiveThresholdPercent=90"
+        "-XX:G1RSetUpdatingPauseTimePercent=5"
+        "-XX:SurvivorRatio=32"
+        "-XX:+PerfDisableSharedMem"
+        "-XX:MaxTenuringThreshold=1"
+      ];
       maxMemory = 8192;
       war = "${pkgs.flake.airsonic-advanced}/webapps/airsonic.war";
     };
