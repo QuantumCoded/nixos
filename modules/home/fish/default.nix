b4ca281af37fa9faef24d2184e3ffeb78843fad2
QuantumCoded <QuantumCoded@users.noreply.github.com>
2024-01-09 07:05:08 -0600

    remove fish ide function

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -11,7 +11,6 @@ in
   options.base.fish = {
     enable = mkEnableOption "Enable Fish";
     tide.enable = mkEnableOption "Fish Tide Theme";
-    funcs.ide.enable = mkEnableOption "IDE Fish Function";
   };
 
   config = mkIf cfg.enable {
@@ -21,12 +20,6 @@ in
       plugins = mkIf cfg.tide.enable [
         { name = "tide"; src = pkgs.fishPlugins.tide.src; }
       ];
-
-      functions = {
-        ide = mkIf cfg.funcs.ide.enable ''
-          ${pkgs.direnv}/bin/direnv exec $argv[1] code $argv[1..]
-        '';
-      };
     };
   };
 }
