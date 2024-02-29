2547a3d64e67714ccc649ea108a037e5834a4467
QuantumCoded <QuantumCoded@users.noreply.github.com>
2024-02-29 12:07:29 -0600

    rename extraOptions for syncthing

--- roles/server.nix
+++ roles/server.nix
@@ -187,7 +187,7 @@ in
     syncthing = {
       enable = true;
       # TODO: add devices
-      extraOptions.gui.insecureSkipHostcheck = true;
+      settings.gui.insecureSkipHostcheck = true;
     };
 
     vsftpd = {
