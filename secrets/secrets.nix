dd0bef2d83be695623c2f98e1ad86479e9d41e86
QuantumCoded <QuantumCoded@users.noreply.github.com>
2024-05-17 17:44:56 -0500

    add host and rekey agenix secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,12 +3,13 @@ let
   jeff = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO0Z+jY49Owc0MeSyZLUgBdfct6PFEUWwvBfBmz0Cyzn";
 
   # Systems
+  avalon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEcFB9THiR0c6K1pfCl+eG1kIPiJ5QZuCA1PbQJdmHT+";
   hydrogen = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOibBFyzpvrT7Q1F1kywc2gIOsog8HdVSUl5IXa1aHQs";
   odyssey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPzbqF8beGqcGqfij9TVBxWZ4wT0t651UYqFKOjSSXr/";
   quantum = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDoCx1wXiAMb01U2JAM5xHmdLlPnd81EITSNn4FDkMvK";
 
   allUsers = [ jeff ];
-  allSystems = [ hydrogen odyssey quantum ];
+  allSystems = [ avalon hydrogen odyssey quantum ];
 
   everywhere = allUsers ++ allSystems;
 in
