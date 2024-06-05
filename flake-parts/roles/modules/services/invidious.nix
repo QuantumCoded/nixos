9b820b5867eb5504b4644a6dc013deebc1b6cf03
QuantumCoded <QuantumCoded@users.noreply.github.com>
2024-06-04 23:07:34 -0500

    switch invidious port to resolve conflict

--- flake-parts/roles/modules/services/invidious.nix
+++ flake-parts/roles/modules/services/invidious.nix
@@ -1,11 +1,12 @@
 {
   services = {
     caddy.virtualHosts."http://invidious.hydrogen.lan".extraConfig = ''
-      reverse_proxy http://127.0.0.1:3000
+      reverse_proxy http://127.0.0.1:3033
     '';
 
     invidious = {
       enable = true;
+      port = 3033;
       # HACK: invidious won't start without this being set
       # a secure key should be used and encrypted
       settings.hmac_key = "eeth5na2aing5xaiP3Uu";
