3c44d1b541eaf4b51658aee7eb8af4fb4fab904c
QuantumCoded <QuantumCoded@users.noreply.github.com>
2024-06-26 14:22:19 -0500

    add ferret to packages

--- flake-parts/packages/default.nix
+++ flake-parts/packages/default.nix
@@ -1,6 +1,7 @@
 {
   perSystem = { inputs', pkgs, ... }: {
     packages = {
+      ferret = pkgs.callPackage ./ferret { };
       gdlauncher = pkgs.callPackage ./gdlauncher.nix { };
       nimbus-roman-ttf = pkgs.callPackage ./nimbus-roman.nix { };
       nomos-rebuild = pkgs.callPackage ./nomos-rebuild { };
