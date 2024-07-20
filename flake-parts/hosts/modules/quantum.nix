ee7effdaeb6cd89ecb254aa3fe6f9030f6ea426a
QuantumCoded <QuantumCoded@users.noreply.github.com>
2024-07-19 23:07:14 -0500

    fix word wrapping of long commands

--- flake-parts/hosts/modules/quantum.nix
+++ flake-parts/hosts/modules/quantum.nix
@@ -9,7 +9,10 @@
       };
 
       extraConfig = ''
-        ${pkgs.procps}/bin/pidof xwinwrap || ${pkgs.self.xwinwrap}/bin/xwinwrap -fs -fdt -ni -b -nf -un -o 1.0 -- ${pkgs.mpv}/bin/mpv -wid WID --loop --no-audio ${../../../wallpapers/animated.mkv}
+        ${pkgs.procps}/bin/pidof xwinwrap \
+        || ${pkgs.self.xwinwrap}/bin/xwinwrap -fs -fdt -ni -b -nf -un -o 1.0 \
+        -- ${pkgs.mpv}/bin/mpv -wid WID --loop --no-audio --no-terminal \
+        ${../../../wallpapers/animated.mkv}
       '';
     };
 
@@ -32,9 +35,17 @@
 
     # TODO: this should adapt to different monitor configurations
     # specifically 1, 3, and 4 monitor(s)
-    services.xserver.displayManager.setupCommands = ''
-      ${pkgs.xorg.xrandr}/bin/xrandr --output DP-0 --mode 1920x1080 --rate 144 --pos 0x0 --brightness 0.5 --output DP-2 --primary --mode 1920x1080 --rate 144 --right-of DP-0 --brightness 0.5 --output DP-4 --mode 1920x1080 --rate 144 --right-of DP-2 --brightness 0.5 --output HDMI-0 --mode 1920x1080 --rate 60 --right-of DP-4 --brightness 1
-    '';
+    services.xserver.displayManager.setupCommands =
+      let
+        brightness = "1";
+      in
+      ''
+        ${pkgs.xorg.xrandr}/bin/xrandr \
+        --output DP-0 --mode 1920x1080 --rate 144 --pos 0x0 --brightness ${brightness} \
+        --output DP-2 --primary --mode 1920x1080 --rate 144 --right-of DP-0 --brightness ${brightness} \
+        --output DP-4 --mode 1920x1080 --rate 144 --right-of DP-2 --brightness ${brightness} \
+        --output HDMI-0 --mode 1920x1080 --rate 60 --right-of DP-4 --brightness 1
+      '';
 
     base.nvidia = {
       enable = true;
