a5462d2a152c0093e67cacd57b2c98f9ca8ca833
QuantumCoded <QuantumCoded@users.noreply.github.com>
2024-07-25 04:16:40 -0500

    move virtualization info into host

--- flake-parts/systems/hosts/modules/quantum.nix
+++ flake-parts/systems/hosts/modules/quantum.nix
@@ -58,9 +58,24 @@
     hardware.bluetooth.enable = true;
     nix.settings.trusted-users = [ "jeff" ];
 
-    base.syncthing = {
-      enable = true;
-      networks = inputs.homelab.syncthingNetworks;
+    base.virtualization = {
+      vfioPciIds = [
+        # USB 3.1 Controller
+        "1b21:1242"
+        # GPU
+        "10de:1e04"
+        # GPU Audio
+        "10de:10f7"
+        # GPU USB 3.1 Controller
+        "10de:1ad6"
+        # GPU USB-C Controller
+        "10de:1ad7"
+      ];
+
+      cpuArch = "intel";
+      nvidiaGpu = true;
+      virtCpus = "6-7";
+      hostCpus = "0-7";
     };
 
     age.secrets = {
