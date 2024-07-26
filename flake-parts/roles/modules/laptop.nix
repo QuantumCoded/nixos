01a77fa036f510f0c73362fc4bb4071abe1bb0db
QuantumCoded <QuantumCoded@users.noreply.github.com>
2024-07-26 01:55:32 -0500

    empty laptop and desktop roles

--- flake-parts/roles/modules/laptop.nix
+++ flake-parts/roles/modules/laptop.nix
@@ -1,18 +1,1 @@
-{ pkgs, ... }:
-
-{
-  # MySQL
-  services.mysql = {
-    enable = true;
-    package = pkgs.mariadb;
-    ensureUsers = [{
-      name = "jeff";
-      ensurePermissions = {
-        "music.*" = "ALL PRIVILEGES";
-        "testing.*" = "ALL PRIVILEGES";
-        "dbms.*" = "ALL PRIVILEGES";
-      };
-    }];
-    ensureDatabases = [ "music" "testing" "dbms" ];
-  };
-}
+{ }
